d414e6eb281259c415d87304d61085ec0b629a94
JosephKhai <josephkhaipi@gmail.com>
2024-04-30 22:17:06 +1000

    Done Server side pagination

--- src/app/pages/city/city.component.ts
+++ src/app/pages/city/city.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { error } from 'console';
+import { Subscription } from 'rxjs';
 import { City } from 'src/app/interface/City';
 import { CityService } from 'src/app/services/city.service';
 
@@ -10,7 +11,7 @@ import { CityService } from 'src/app/services/city.service';
   templateUrl: './city.component.html',
   styleUrls: ['./city.component.scss'],
 })
-export class CityComponent implements OnInit {
+export class CityComponent implements OnInit, OnDestroy {
 
   cities!: MatTableDataSource<City>;
   displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
@@ -19,15 +20,47 @@ export class CityComponent implements OnInit {
 
   constructor(private cityService: CityService) { }
 
+  subscribtion!: Subscription;
 
-
-  ngOnInit() {
-
-    this.cityService.getCities().subscribe(data => {
-      this.cities = new MatTableDataSource<City>(data);
-      this.cities.paginator = this.paginator;
-    }, error => console.log(error));
+  ngOnDestroy(): void {
 
   }
+
+
+
+  ngOnInit(): void {
+    // this.cityService.getCities().subscribe(data => {
+    //   this.cities = new MatTableDataSource<City>(data);
+    //   this.cities.paginator = this.paginator;
+    // }, error => console.log(error));
+
+    
+    var pageEvent = new PageEvent();
+    pageEvent.pageIndex = 0;
+    pageEvent.pageSize = 10;
+    this.getData(pageEvent);
+
+  
+  }
+
+/**
+ * Fetches data from the server based on the given page event.
+ *
+ * @param event - The page event that contains information about the page and the page size.
+ */
+getData(event: PageEvent): void {
+
+  this.cityService.getData(event).subscribe(result => {
+
+    console.log(result);
+    this.paginator.length = result.totalCount;
+    this.paginator.pageIndex = result.pageIndex;
+    this.paginator.pageSize = result.pageSize;
+
+    this.cities = new MatTableDataSource<City>(result.data);
+  }, error => console.log(error));
+}
+
+  
 
 }
--- src/app/services/city.service.ts
+++ src/app/services/city.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { City } from '../interface/City';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import { PageEvent } from '@angular/material/paginator';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +16,18 @@ export class CityService implements OnInit {
 
   }
 
-  getCities(): Observable<City[]> {
-    return this.http.get<City[]>(environment.baseUrl + 'api/city');
+  getCities(): Observable<City> {
+    return this.http.get<City>(environment.baseUrl + 'api/city');
+  }
+
+  getData(event: PageEvent): Observable<any> {
+    var url = environment.baseUrl + 'api/city/GetAllCitiesPagination';
+    var params = new HttpParams()
+    .set("pageIndex", event.pageIndex)
+    .set("pageSize", event.pageSize);
+
+    return this.http.get<any>(url, {params});
+      
   }
 
   
